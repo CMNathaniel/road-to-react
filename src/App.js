6384f0ed010594e04f4a74bbd304140d18545965
Christine Nathaniel <cnathaniel@paycor.com>
2022-07-21 08:52:47 -0700

    fundamental of react, react component declaration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://reduxjs.org/',
+    author: 'Dan Abramov',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  },
+  {
+    title: 'Meemu',
+    url: 'https://google.com/',
+    author: 'Stine Nathanihan',
+    num_comments: 77,
+    points: 7,
+    objectID: 2
+  }
+];
 
-export default App;
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+    <hr />
+  <Search />
+  <List />
+  </div>
+);
+
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
+
+const List = () => (
+  <ul>
+      {list.map((item) => (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>Title:{item.title},</span>
+            <span>Comment Count:{item.num_comments},</span>
+            <span>Author: {item.author}</span>
+          </li>
+        ))}
+  </ul>
+);
+
+export default App; 
